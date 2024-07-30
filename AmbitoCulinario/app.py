3b4c7d3b3340353bb98fb5ad96fc355f22d93d07
MeLaZaco <frank.rojas.arbayza@alumnos.sip.cl>
2024-07-30 15:20:20 -0400

    dfsd

--- AmbitoCulinario/app.py
+++ AmbitoCulinario/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from models.informacion_productos import Producto
 
 app = Flask(__name__)
